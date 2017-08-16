48b710905094152174df40543cc267d7d36f9fa2
temaEmelyan <tema.emelyan@gmalil.com>
2017-08-17 02:05:32 +1000

    trying to switch to java config 1

--- src/main/java/ru/javawebinar/topjava/config/SpringAppConfig.java
+++ src/main/java/ru/javawebinar/topjava/config/SpringAppConfig.java
@@ -2,7 +2,9 @@ package ru.javawebinar.topjava.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import(SpringToolsConfig.class)
 @ComponentScan({"ru.**.service", "ru.**.web"})
 @Configuration
 public class SpringAppConfig {
--- src/main/java/ru/javawebinar/topjava/config/SpringToolsConfig.java
+++ src/main/java/ru/javawebinar/topjava/config/SpringToolsConfig.java
@@ -5,9 +5,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 @EnableCaching
+@Configuration
 public class SpringToolsConfig {
 
     @Bean
